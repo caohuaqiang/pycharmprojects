d856aa4f38339090f7a8793d23e8983350dee7b9
caohuaqiang <342473195@qq.com>
2018-04-18 22:37:04 +0800

    定位方式改造以及参数配置化改造

--- test_case/deliver.py
+++ test_case/deliver.py
@@ -1,6 +1,7 @@
+# coding = utf-8
 from selenium import webdriver
 import time
-# coding = utf-8
+
 
 class FB:
     def __init__(self, config: dict):
@@ -18,34 +19,33 @@ class FB:
         driver.find_element_by_id("password").send_keys("123456")
         driver.find_element_by_id("valicode").clear()
         driver.find_element_by_id("valicode").send_keys("jfcf")
-        driver.find_element_by_xpath("html/body/div[1]/div/form/ul/li[5]/input").click()
+        driver.find_element_by_css_selector('[value=立即登录]').click()
         time.sleep(5)
 
     def Fa_Biao(self):
         driver = self.driver
         self.login()
-
-        chq = driver.find_elements_by_xpath('//*[@id="nav"]/div[3]/div[1]/div[1]')
-        for x in chq:
+        for x in driver.find_elements_by_css_selector('.panel-title.panel-with-icon'):
             if x.text == '借贷管理':
                 x.click()
-
         time.sleep(2)
         driver.find_element_by_partial_link_text(u'借款初审').click()
 #===============================================================================================================================================
-        iframe_jiekuanchushen = "/html/body/div/div[2]/div/div/div[2]/div[2]/div/iframe"
-        driver.switch_to.frame(driver.find_element_by_xpath(iframe_jiekuanchushen))  # 跳转到借款初审页面的iframe框架内
+        iframe_jiekuanchushen = 'src="/modules/loan/borrow/verifyBorrowManager.html"'
+        driver.switch_to.frame(driver.find_element_by_css_selector('[%s]' % iframe_jiekuanchushen))     # 跳转到借款初审页面的iframe框架内
         driver.find_element_by_id("a").click()  # 发标
         time.sleep(1)
 
-        iframe_tankuang = "/html/body/div[3]/div[2]/iframe"
-        driver.switch_to.frame(driver.find_element_by_xpath(iframe_tankuang))  # 跳转到弹框页面(ifram嵌套，从第一层跳到第二层)
-
+        iframe_tankuang = 'src="/modules/loan/borrow/borrowAddPage.html?type=112"'
+        driver.switch_to.frame(driver.find_element_by_css_selector('[%s]' % iframe_tankuang))   # 跳转到弹框页面(ifram嵌套，从第一层跳到第二层)
 #-----------------------------------发标表单------------------------------------------------------------------------------------------------------
-        # driver.find_element_by_xpath("html/body/form/div[1]/div/div[3]/i").click()  # 活动标
-        driver.find_element_by_xpath("/html/body/form/div/div/div[2]/i").click()  # 热销中
 
-        activity_list = self.config['activity']
+        type = driver.find_elements_by_css_selector('.layui-unselect.layui-form-radio')
+        for a in type:                                                                          # 标的类型
+            if a.text[1:] == self.config['type']:
+                a.click()
+
+        activity_list = self.config['activity']                                                 # 运营活动
         activity = driver.find_elements_by_css_selector('.layui-unselect.layui-form-checkbox')
         for a in activity:
             if a.text.split('\n')[0] in activity_list:
@@ -53,17 +53,17 @@ class FB:
         print('--------------------------------------------')
 
         if self.config['is_recommend'] == 1:
-            driver.find_element_by_xpath('/html/body/form/div[1]/div/div[5]/span').click()
+            driver.find_element_by_css_selector('.layui-unselect.layui-form-checkbox>span').click()
         driver.find_element_by_id("ads").send_keys(self.config['bm'])  # 标名
-        driver.find_element_by_xpath("/html/body/form/div[2]/div[2]/div/input").send_keys(self.config['product_number'])  # 项目编号
+        driver.find_element_by_css_selector('[name="borrowNo"]').send_keys(self.config['product_number'])  # 项目编号
 
         red_path = '//*[@lay-value="%s"]' % self.config['redpacket']
         coupon_path = '//*[@lay-value="%s"]' % self.config['coupon']
-        driver.find_element_by_xpath("/html/body/form/div[3]/div/div/div/div/input").click()  # 弹出红包方案下拉框
+        driver.find_element_by_css_selector('[value="请选择红包方案"]').click()    # 弹出红包方案下拉框
         time.sleep(0.5)
         driver.find_element_by_xpath(red_path).click()
 
-        driver.find_element_by_xpath("/html/body/form/div[3]/div[2]/div/div/div/input").click()  # 弹出加息券方案下拉框
+        driver.find_element_by_css_selector('[value="请选择加息券方案"]').click()   # 弹出加息券方案下拉框
         time.sleep(0.5)
         driver.find_element_by_xpath(coupon_path).click()
 
@@ -88,20 +88,17 @@ class FB:
 if __name__ == '__main__':
     config = {'bm': 'chq',
               'product_number': 'CHQ888',
-              'is_recommend': 0,
+              'type': '定期标',
+              'is_recommend': 1,
               'redpacket': 'chq专用红包方案',
               'coupon': 'chq专用加息券方案',
               'activity': ['参与大额返现', '变态活动'],
               'money': '500000',
               'apr': '10',
               'timelimit': '60',
-
               }
     fb = FB(config)
     fb.Fa_Biao()
-
-
-
 
 
 
